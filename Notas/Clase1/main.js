0c1960c085d77a0e5575e47fa2b9b66399364d82
Matias Sanguinet <rmsrcel@gmail.com>
2023-07-06 20:21:02 -0300

    primer commit

---
+++ Notas/Clase1/main.js
@@ -0,0 +1,50 @@
+//comentario en linea
+
+/*
+comentario
+de
+bloque
+*/
+//SE UTILIZA camelCase para nombres de variables
+
+//recomendable para declarar variables (USAR ESTA MANERA)
+let edad = 10;
+let nombre = "prueba con comillas la variable es string";//siempre termina en ;
+
+//NO recomendable para declarar varaibles
+var cantidad = 6;
+
+//CONSTANTES, ES RECOMENDABLE USAR MAYUSCULAS para el nombre
+const NUMERO = 5;
+
+
+
+//tipos de datos
+//number
+let edad2 = 10;
+
+//string
+let varString = "10";
+
+
+//ejemplos de operaciones
+
+let numeroUno = 5;
+let numeroDos = 6;
+
+let suma = numeroUno + numeroDos; //suma de dos nros
+let resta = numeroUno - numeroDos;
+let mul = numeroUno * numeroDos;
+let div = numeroUno / numeroDos; 
+let mod = numeroUno % numeroDos; 
+
+console.log("suma",suma);//console.log muestra en consola
+console.log("resta",resta);
+console.log("mul",mul);
+console.log("div" , div);
+console.log("mod",mod); //si se pone , lo concatena con espacio
+console.log("mod"+mod); //si se pone un + lo concatena sin espacio
+//nota, con ALT y flecha se puede mover algo seleccionado
+
+
+
