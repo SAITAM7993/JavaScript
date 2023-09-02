43ce4f9d68d1814922d4c37dc06425023ef9cb7d
Matias Sanguinet <rmsrcel@gmail.com>
2023-09-02 11:29:32 -0300

    cambios en entrega final

--- Entregas/EntregaFinal-Sanguinet/js/logica-interaccion.js
+++ Entregas/EntregaFinal-Sanguinet/js/logica-interaccion.js
@@ -62,6 +62,57 @@ function resumenAutoSeleccionado(auto) {
   )
   return autoResumen;
 }
+
+/**************************************************
+IMPRESION PDF
+**************************************************/
+function descargarPDF(autoResumen) {   
+  window.jsPDF = window.jspdf.jsPDF;
+  const doc = new jsPDF();
+
+  let img = new Image();
+
+  img.src = `./EntregaFinal-Sanguinet/${autoResumen.imgColor}`; //asi funciona, se le pasa la ruta de la img
+  doc.addImage(img, 'JPEG', 157, 100, 40, 40); //addimage (img, x, y, width, height, alias, compresion, rotacion)
+
+  img.src = `./EntregaFinal-Sanguinet/${autoResumen.imgInterior}`; //asi funciona, se le pasa la ruta de la img
+  doc.addImage(img, 'JPEG', 157, 145, 40, 40); 
+
+  img.src = `./EntregaFinal-Sanguinet/${autoResumen.imgPerfil}`; 
+  doc.addImage(img, 'JPEG', 15, 100, 137, 85); 
+
+  let y = 15; //coordenada y de pdf
+  let x = 15; //coordenada x de pdf
+  doc.setFontSize(32); //Si quiero cambiar el tamaño de fuente lo tengo que hacer antes de el text (y luego volver a cambiar en caso de que no quiera seguir con ese tamaño)
+  doc.text("Simulador AUDIX", x, y);
+ 
+  x += 10;
+  y += 20;
+
+  doc.autoTable({
+    html: '#tabla-resumen', //si obtengo tabla de html le pongo el id de esta
+    startY: y, //donde comenzar la tabla eje Y
+    styles: {
+      fontSize: 14, //tamaño fuente
+      cellWidth: 'wrap' 
+    },
+    theme: 'striped', //tema de la tabla 'striped'|'grid'|'plain'
+    headStyles: { //estilos de cabezales
+      fillColor: [0, 0, 0]  //negro, solo sirve rgb
+    }
+
+  }); //toma la tabla del id que le diga, autotablela deja bien armada
+
+  doc.line(15, 270, 200, 270); //line(x1, y1, x2, y2, style) dibuja linea
+  let fecha = new Date();  
+  doc.setFontSize(10);
+  doc.text(fecha.toLocaleString(), 160, 280); 
+  doc.output('dataurlnewwindow'); //esto abre en una pestaña nueva
+  //doc.save('table.pdf'); //esto lo guarda
+} 
+
+
+
 /**************************************************
 FUCNIONES - FIN
 **************************************************/
@@ -144,52 +195,3 @@ window.onclick = function (event) {
 /**************************************************
 EVENTOS (otros eventos para botones y modals) - FIN
 **************************************************/
-/**************************************************
-IMPRESION PDF
-**************************************************/
-function descargarPDF(autoResumen) {   
-  window.jsPDF = window.jspdf.jsPDF;
-  const doc = new jsPDF();
-
-  let img = new Image();
-
-  img.src = `./EntregaFinal-Sanguinet/${autoResumen.imgColor}`; //asi funciona, se le pasa la ruta de la img
-  doc.addImage(img, 'JPEG', 157, 100, 40, 40); //addimage (img, x, y, width, height, alias, compresion, rotacion)
-
-  img.src = `./EntregaFinal-Sanguinet/${autoResumen.imgInterior}`; //asi funciona, se le pasa la ruta de la img
-  doc.addImage(img, 'JPEG', 157, 145, 40, 40); 
-
-  img.src = `./EntregaFinal-Sanguinet/${autoResumen.imgPerfil}`; 
-  doc.addImage(img, 'JPEG', 15, 100, 137, 85); 
-
-  let y = 15; //coordenada y de pdf
-  let x = 15; //coordenada x de pdf
-  doc.setFontSize(32); //Si quiero cambiar el tamaño de fuente lo tengo que hacer antes de el text (y luego volver a cambiar en caso de que no quiera seguir con ese tamaño)
-  doc.text("Simulador AUDIX", x, y);
- 
-  x += 10;
-  y += 20;
-
-  doc.autoTable({
-    html: '#tabla-resumen', //si obtengo tabla de html le pongo el id de esta
-    startY: y, //donde comenzar la tabla eje Y
-    styles: {
-      fontSize: 14, //tamaño fuente
-      cellWidth: 'wrap' 
-    },
-    theme: 'striped', //tema de la tabla 'striped'|'grid'|'plain'
-    headStyles: { //estilos de cabezales
-      fillColor: [0, 0, 0]  //negro, solo sirve rgb
-    }
-
-  }); //toma la tabla del id que le diga, autotablela deja bien armada
-
-  doc.line(15, 270, 200, 270); //line(x1, y1, x2, y2, style) dibuja linea
-  let fecha = new Date();  
-  doc.setFontSize(10);
-  doc.text(fecha.toLocaleString(), 160, 280); 
-  doc.output('dataurlnewwindow'); //esto abre en una pestaña nueva
-  //doc.save('table.pdf'); //esto lo guarda
-} 
-
-
--- Entregas/EntregaFinal-Sanguinet/js/main.js
+++ Entregas/EntregaFinal-Sanguinet/js/main.js
@@ -7,41 +7,51 @@ const obtenerAutosFetch = async () => {
         if (!JSON.parse(localStorage.getItem("autos"))){ //si autos esta vacio (en local storage)
             const response = await fetch("./db/autos.json"); //obtengo del json simulando api
             const listaAutos = await response.json();          
-            localStorage.setItem("autos", JSON.stringify(listaAutos));//lo cargo en localStorage
-        }    
+            localStorage.setItem("autos", JSON.stringify(listaAutos));//lo cargo en localStorage            
+        }
     } catch (error) { 
         console.log(error);
     }
 }
-
 export function obtenerAutos(){
     obtenerAutosFetch()
         .then(() => {
-            const autos = JSON.parse(localStorage.getItem("autos")); //obtengo la lista de autos del local storage                
-            cargarOpciones(autos, 0); //cargo todas las opciones
-            cargarEventoModelos(autos); //cargo los eventos 
-           
+            const autos = JSON.parse(localStorage.getItem("autos")); //obtengo la lista de autos del local storage   
+            if (sessionStorage.getItem("modelo")) {                
+                cargarOpciones(autos, 0); //si le paso algo diferente a 0 rompe
+                cargarEventoModelos(autos);
+                console.log(sessionStorage.getItem("modelo"));
+                
+            } else {
+                
+                cargarOpciones(autos, 0); //cargo todas las opciones
+                cargarEventoModelos(autos); //cargo los eventos 
+                
+            }           
         });
 }
+
 /**************************************************
 FUNCIONES PARA CARGAR DOM, LIMPIAR, AGREGAR EVENTOS 
 **************************************************/
 //FUNCION PARA CARGAR evento click en las img de modelo
 function cargarEventoModelos(autos) {
+    
     //obtengo el html que contiene los modelos
     let listaModelos = document.getElementById("modelsContainer");
     //obtengo las img/input del html que contiene los modelos
     let input = listaModelos.getElementsByTagName("input");    
     for (let item of input) { //recorro las img/input para agregarle un evento        
-        item.addEventListener('click', () => {                         
+        item.addEventListener('click', () => {             
             let opcModelo = item.value //obtengo el valor numerico
             //tomo el html que contiene las img del preview
             let listaPreview = document.getElementById("car-preview");
             //tomo solo las img
             let imgPreview = listaPreview.getElementsByTagName("img");    
             //recorro los tags img
-            for (let itemImg of imgPreview) {
-                //le cambio el atributo src  
+            for (let itemImg of imgPreview) {   
+                //le cambio el atributo src 
+                
                 autos.forEach((item) => { //recorro los autos
                     //si el modelo del auto coincide con el seleccionado Y el item que estoy es el de perfil asigno la img de perfil (src)
                     //si el modelo del auto coincide con el seleccionado Y el item que estoy es el de perfil asigno la img de frente (src)
@@ -69,11 +79,11 @@ function cargarEvento(categoria, autos){
     for (let item of input) {        
         item.addEventListener('click', () => {           
             sessionStorage.setItem(categoria, item.value);           
-            if (categoria === "color") { //no uso operador avanzado porque es mucho codigo                
+            if (categoria === "color") { //no uso operador avanzado porque es mucho codigo, 
+                //esto es para setear las IMG del preview del costado izquierdo (perfil y frente)       
                 let modelo = sessionStorage.getItem("modelo");
                 let color = sessionStorage.getItem("color");                
-                padre = document.getElementById
-                ("car-preview");                
+                padre = document.getElementById("car-preview");                
                 padre.innerHTML = `<img class="image-xl" src="./EntregaFinal-Sanguinet/${autos[modelo].colores[color].imgB}" alt="${autos[modelo].colores[color].nombre}" id = "carPreview-perfil">`;
                 padre.innerHTML += `<img class="image-xl" src="./EntregaFinal-Sanguinet/${autos[modelo].colores[color].imgC}" alt="${autos[modelo].colores[color].nombre}" id = "carPreview-frente">`;
             }
@@ -81,12 +91,13 @@ function cargarEvento(categoria, autos){
     }
 }
 
-const cargarOpciones = (autos, modeloSel) => {
+const cargarOpciones = (autos, modeloSel) => {       
     //PRE CARGO INTERIORES    
     //obtengo el ID del padre donde voy a agregar los modelos (es un div con id modelsContainer)
     let padre = document.getElementById("modelsContainer");  
-    
-    if (modeloSel === 0) {
+    //padre.innerHTML = '';
+    if (modeloSel === 0) { //por alguna razon esto funciona solo si esta dentro del if..
+        padre.innerHTML = '';
         //solo si el modelo seleccionado es el 0 vuelvo a cargar los modelos (para intentar optimizar la carga)
         //CARGO MODELOS        
         autos.forEach((item) => {
@@ -95,11 +106,13 @@ const cargarOpciones = (autos, modeloSel) => {
                         <img class="image image-l" src="./EntregaFinal-Sanguinet/${item.colores[0].imgB}" id="imgModel${item.id}">`;//por alguna razon no sabe en donde estoy parado y le tengo que agregar que entre a  la carpeta de preentrega3
             padre.appendChild(modelo);
         });
-       sessionStorage.setItem("modelo", 0);
-       sessionStorage.setItem("color", 0);
-       sessionStorage.setItem("interior", 0);    
-   }
+       
+  }
 
+    sessionStorage.setItem("modelo", 0);
+       sessionStorage.setItem("color", 0);
+    sessionStorage.setItem("interior", 0);    
+    
     //CARGO INTERIORES
     padre = document.getElementById("interiorContainer");
     padre.innerHTML = "";
@@ -144,29 +157,6 @@ function limpiar() {
     sessionStorage.setItem("color", 0);
     sessionStorage.setItem("interior", 0);
 }
-
-function cargarOpcionesDesdeSession() { 
-    let opcModelo = sessionStorage.getItem("modelo");
-    //tomo el html que contiene las img del preview
-    let listaPreview = document.getElementById("car-preview");
-    //tomo solo las img
-    let imgPreview = listaPreview.getElementsByTagName("img");    
-    //recorro los tags img
-    for (let itemImg of imgPreview) {
-        //le cambio el atributo src  
-        autos.forEach((item) => { //recorro los autos
-            //si el modelo del auto coincide con el seleccionado Y el item que estoy es el de perfil asigno la img de perfil (src)
-            //si el modelo del auto coincide con el seleccionado Y el item que estoy es el de perfil asigno la img de frente (src)
-            //luego si el modelo del auto coincide con el seleccionado le seteo el src                   
-            let src = "";
-            (item.id === parseInt(opcModelo) && itemImg.id === "carPreview-perfil") && (src = item.colores[0].imgB);
-            (item.id === parseInt(opcModelo) && itemImg.id === "carPreview-frente") && (src = item.colores[0].imgC);
-            item.id === parseInt(opcModelo) && itemImg.setAttribute("src", `./EntregaFinal-Sanguinet/${src}`);           
-        });     
-    }
-    cargarOpciones(autos, opcModelo);
-}
-
 //RESTABLECER
 //Obtengo el id del boton restablecer
 const btnRestablecer = document.getElementById("restablecer");
@@ -174,6 +164,8 @@ btnRestablecer.onclick = function () {
     limpiar(); //borro todo el contenido de modelos, preview, colores, interiores y luego lo vuelvo a precargar  
     obtenerAutos();
 }
+
+
 /**************************************************
 FUNCIONES PARA CARGAR DOM, LIMPIAR, AGREGAR EVENTOS - FIN 
 **************************************************/
